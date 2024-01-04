5acc6d8e10163316e2dd58fa429efff6a78815f3
Tim Jonsson <tim.jonsson@easypark.net>
2024-01-04 11:46:00 +0100

    Creating AnswerButton- and questions widget and importing questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
-  
+
   @override
   State<QuestionScreen> createState() {
     return _QuestionScreenState();
@@ -12,6 +13,14 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Text("Questionsdata");
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnswerButton(answerText: "Answer 1", onTap: () {}),
+        const SizedBox(height: 10),
+        AnswerButton(answerText: "Answer 2", onTap: (){})
+      ],
+    ));
   }
 }
