c7b8810a8a3fc4326c91cc3b8e3e2da043419723
Tim Jonsson <tim.jonsson@easypark.net>
2024-01-07 17:25:10 +0100

    Adding and showing a results screen after the last question

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/main_page_container.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
-  
+
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -12,26 +14,56 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  var activeScreen = 'main-page-container';
-  final List<String> selectedAnswers = [];
+  static const questionsScreen = 'questions-screen';
+  static const mainPageContainer = 'main-page-container';
+  static const resultsScreen = 'results-screen';
+
+  var activeScreen = mainPageContainer;
+  List<String> selectedAnswers = [];
 
   void switchScreen() {
     setState(() {
-      activeScreen = 'questions-screen';
+      activeScreen = questionsScreen;
     });
   }
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = resultsScreen;
+        selectedAnswers = [];
+      });
+    }
+  }
+
+  Widget screenToShow(screen) {
+    Widget screenWidget;
+
+    switch (screen) {
+      case questionsScreen:
+        screenWidget = QuestionScreen(onSelectedAnswer: chooseAnswer);
+        break;
+      case resultsScreen:
+        screenWidget = const ResultsScreen();
+        break;
+      default:
+        screenWidget = MainPageContainer(switchScreen);
+    }
+
+    return screenWidget;
   }
 
   @override
   Widget build(context) {
+    Widget screenWidget = screenToShow(activeScreen);
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
           color: Colors.purple,
-          child: activeScreen == 'main-page-container' ? MainPageContainer(switchScreen): QuestionScreen(onSelectedAnswer: chooseAnswer),
+          child: screenWidget,
         ),
       ),
     );
